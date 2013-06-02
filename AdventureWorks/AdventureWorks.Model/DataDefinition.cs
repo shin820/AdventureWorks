a44f696a1c995e4e896887573754a9d6473c88d6
shin820@163.com <shin820@163.com>
2013-06-02 22:31:15 +0800

    Add EmployeeEditViewModelTest

---
+++ AdventureWorks/AdventureWorks.Model/DataDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.Model
+{
+    public static class DataDefinition
+    {
+        public static class MartialStatus
+        {
+            public static string Married = "M";
+            public static string UnMarried = "S";
+        }
+
+        public static class Gender
+        {
+            public static string Male = "M";
+            public static string Female = "F";
+        }
+    }
+}
---
+++ AdventureWorks/AdventureWorks.UnitTest/ViewModel/EmployeeEditViewModelTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureWorks.Model;
+using AdventureWorks.WebUI.MVC.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventureWorks.UnitTest.ViewModel
+{
+    [TestClass]
+    public class EmployeeEditViewModelTest
+    {
+        [TestMethod]
+        public void ShouldGetMartialStatusList()
+        {
+            var viewModel = new EmployeeEditViewModel();
+            viewModel.MaritalStatus = DataDefinition.MartialStatus.UnMarried;
+
+            Assert.AreEqual(DataDefinition.MartialStatus.UnMarried, viewModel.MartialStatusList.SelectedValue);
+        }
+
+        [TestMethod]
+        public void ShouldGetGenderList()
+        {
+            var viewModel = new EmployeeEditViewModel();
+            viewModel.Gender = DataDefinition.Gender.Female;
+
+            Assert.AreEqual(DataDefinition.Gender.Female, viewModel.GenderList.SelectedValue);
+        }
+
+        [TestMethod]
+        public void ShouldGetSalaryTypeList()
+        {
+            var viewModel = new EmployeeEditViewModel();
+            viewModel.SalariedFlag = true;
+
+            Assert.AreEqual(true, viewModel.SalaryTypeList.SelectedValue);
+        }
+    }
+}
